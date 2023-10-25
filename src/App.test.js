3f25666f3e0066a592075608b17f6c7a091c4100
daniel <garcia.daniel609025@gmail.com>
2023-10-25 10:20:34 -0700

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import axios from "axios"
+import dogs from "./_testCommon"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+
+test('renders learn react link', async () => {
+  axios.get.mockResolvedValue({ data: dogs });
+  const { container } = render(<App />);
+  const heading = await waitFor(() => screen.findByText('Welcome!'));
+  const nav = container.querySelector("nav")
+  expect(nav).toBeInTheDocument()
+
+  expect(heading).toBeInTheDocument();
+  expect(axios.get).toHaveBeenCalled()
 });
